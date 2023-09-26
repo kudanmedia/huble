79fc297c760de28478a102881f0c1394f4e20ec2
Kudakwashe Nyengera <mckuda@gmail.com>
2023-09-26 10:43:37 +0200

    resuable label

--- src/components/atoms/Label.js
+++ src/components/atoms/Label.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { cva } from "class-variance-authority";
 
-export default function label() {
-  return (
-    <>
-    
-    </>
-  );
-}
+const label = cva("label", {
+  variants: {
+    intent: {
+      subHeading: ["text-base", "font-semibold", "leading-7", "text-primary"],
+      heading: ["text-4xl", "font-normal", "tracking-tight", "text-white", "sm:text-6xl"],
+      description: ["text-base", "leading-8", "text-white"],
+    },
+  },
+  defaultVariants: {
+    intent: "description",
+  },
+});
+
+const Label = ({ text, intent }) => {
+  return <label className={label({ intent })}>{text}</label>;
+};
+
+export default Label;
