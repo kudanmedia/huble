46021c8008a702aa186fbbc4afb8d6279a3c9787
Kudakwashe Nyengera <mckuda@gmail.com>
2023-09-26 09:17:16 +0200

    clean up and added cva for atomic design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgba(255,90,95,1.0)",
+        secondary: "#333333",
+        white: "rgba(255, 255, 255, 1)",
+        orange: "rgba(238, 87, 94, 1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        circular: ["CircularStd", "sans-serif"]
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px", 
+    },
   },
   plugins: [],
 }
