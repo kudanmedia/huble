b87a72a4391578227ed46562fab22d0fad495f47
Kudakwashe Nyengera <mckuda@gmail.com>
2023-09-26 09:57:10 +0200

    resuable atoms

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { cva } from "class-variance-authority";
 
-export default function button() {
-  return (
-    <>
-    
-    </>
-  );
-}
+const button = cva("button", {
+    variants: {
+      intent: {
+        primary: [
+          "rounded-full", 
+          "bg-primary",
+           "text-sm", 
+           "font-semibold", 
+           "text-white", 
+           "shadow-sm", 
+           "hover:bg-primary/80", 
+        ],
+        secondary: [
+          "rounded-full", 
+           "text-sm", 
+           "font-semibold", 
+           "text-white", 
+           "shadow-sm", 
+           "hover:bg-primary/80",
+           "outline", "outline-primary",
+           "hover:text-black"
+        ],
+      },
+      size: {
+        small: ["py-2", "px-4"],
+        medium: ["px-8", "py-4"],
+      },
+    },
+    compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
+    defaultVariants: {
+      intent: "primary",
+      size: "medium",
+    },
+  });
+  
+  export default function ({ label, intent, size }) {
+    return <button className={button({ intent, size })}>{label}</button>;
+  };
--- src/components/atoms/Label.js
+++ src/components/atoms/Label.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cva } from "class-variance-authority";
 
 export default function label() {
   return (
