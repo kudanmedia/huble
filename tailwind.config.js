1fd4d217ddb7ad94fb194b8713383dc6165b0adf
Kudakwashe Nyengera <mckuda@gmail.com>
2023-09-27 15:11:02 +0200

    added font-awsome and changed tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,14 +16,6 @@ module.exports = {
         circular: ["CircularStd", "sans-serif"]
       },
     },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px", 
-    },
   },
   plugins: [],
 }
